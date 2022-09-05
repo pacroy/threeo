b03fd848f4851bde53a07b418c354eb2c1f6025e
Par <pacroy@users.noreply.github.com>
2022-09-05 20:21:10 +0700

    Redirect with URI

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import { createServer } from 'http'
-const server = createServer((req, res) => {
-  res.write('Hello, world!')
-  res.end()
+const http = require('http')
+const server = http.createServer((req, res) => {
+  console.log(`URL: ${req.url}`)
+  res
+    .writeHead(307, {
+      'Location': `https://github.com/search${req.url}`
+    })
+    .end()
 })
 
 server.listen(3000)
