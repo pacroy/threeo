a43e7c0b62763d2b0f2f44532c8b360b073cf7e3
Par <pacroy@users.noreply.github.com>
2022-09-05 19:57:35 +0700

    Use http to listen on port 3000

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!')
+const http = require('http')
+const server = http.createServer((req, res) => {
+  res.write('Hello, world!')
+  res.end()
+})
+
+server.listen(3000)
+console.log('Listening on port 3000 ...')
