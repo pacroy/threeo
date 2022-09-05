07cf64b149c0898a357804a602ed23c9dff775f8
Par <pacroy@users.noreply.github.com>
2022-09-05 19:59:06 +0700

    Use ES6 to import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
+import { createServer } from 'http'
+const server = createServer((req, res) => {
   res.write('Hello, world!')
   res.end()
 })
